c1f47b1c508c777a02b6890dc52e7b886e718e6f
Shubham kumar <122076501+Shubhamkumartrichy@users.noreply.github.com>
2023-09-28 14:58:22 +0530

    Time: 14 ms (24.85%), Space: 13.7 MB (19.59%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        int col0 = 1,n = matrix.size(),m = matrix[0].size();
+        int col0 = 1;
     // step 1: Traverse the matrix and
     // mark 1st row & col accordingly:
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
+    for (int i = 0; i < matrix.size(); i++) {
+        for (int j = 0; j < matrix[0].size(); j++) {
             if (matrix[i][j] == 0) {
                 // mark i-th row:
                 matrix[i][0] = 0;
@@ -20,8 +20,8 @@ public:
     }
 
     // Step 2: Mark with 0 from (1,1) to (n-1, m-1):
-    for (int i = 1; i < n; i++) {
-        for (int j = 1; j < m; j++) {
+    for (int i = 1; i < matrix.size(); i++) {
+        for (int j = 1; j < matrix[0].size(); j++) {
             if (matrix[i][j] != 0) {
                 // check for col & row:
                 if (matrix[i][0] == 0 || matrix[0][j] == 0) {
@@ -33,12 +33,12 @@ public:
 
     //step 3: Finally mark the 1st col & then 1st row:
     if (matrix[0][0] == 0) {
-        for (int j = 0; j < m; j++) {
+        for (int j = 0; j < matrix[0].size(); j++) {
             matrix[0][j] = 0;
         }
     }
     if (col0 == 0) {
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < matrix.size(); i++) {
             matrix[i][0] = 0;
         }
     }
